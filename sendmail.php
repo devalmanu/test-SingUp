1ef47ec838ee9de047208ac68a23a6328840b6f5
Valiantsina Andreyeva <manuilova-1986@mail.ru>
2022-07-18 13:54:28 +0300

    init: task form-sing up

---
+++ sendmail.php
@@ -0,0 +1,62 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true);
+
+//От кого письмо
+$mail->setFrom('New user!', 'www@www.ru');
+//Кому отправить
+$mail->addAddress('post-email!');
+//Тема письма
+$mail->Subject = 'Hello! This is new user';
+
+//Рука
+$gender = "female";
+if ($_POST['gender'] == "male") {
+	$gender = "male";
+}
+
+//Тело письма
+$body = '<h1>Hello!</h1>';
+
+if (trim(!empty($_POST['name']))) {
+	$body .= '<p><strong>First Name:</strong> ' . $_POST['name'] . '</p>';
+}
+if (trim(!empty($_POST['surname']))) {
+	$body .= '<p><strong>Last Name:</strong> ' . $_POST['name'] . '</p>';
+}
+if (trim(!empty($_POST['email']))) {
+	$body .= '<p><strong>E-mail:</strong> ' . $_POST['email'] . '</p>';
+}
+if (trim(!empty($_POST['gender']))) {
+	$body .= '<p><strong>Gender:</strong> ' . $gender . '</p>';
+}
+if (trim(!empty($_POST['age']))) {
+	$body .= '<p><strong>Возраст:</strong> ' . $_POST['age'] . '</p>';
+}
+
+if (trim(!empty($_POST['message']))) {
+	$body .= '<p><strong>Сообщение:</strong> ' . $_POST['message'] . '</p>';
+}
+
+$mail->Body = $body;
+
+//Отправляем
+if (!$mail->send()) {
+	$message = 'Ошибка';
+} else {
+	$message = 'Регистрация прошла успешно!';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
